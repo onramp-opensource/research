5f45ea3c00265bae58a0299665c5cd7d18390514
Ubuntu <ubuntu@ip-172-26-15-138.us-west-2.compute.internal>
2021-05-04 20:22:44 +0000

    adding a database pass

--- data/data.php
+++ data/data.php
@@ -4,7 +4,7 @@
   $servername = "localhost";
   $database = "crypto";
   $username = "root";
-  $password = "";
+  $password = "onramp1!";
   // Create connection
   $conn = mysqli_connect($servername, $username, $password, $database);
   // Check connection
